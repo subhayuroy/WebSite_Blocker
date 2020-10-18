4d0cdccd7be8f07c7b534f20c87182e30ed34ebe
subhayuroy <subhayu.roy2001@gmail.com>
2020-10-18 12:16:13 +0530

    Creating an empty widget

--- blocker.py
+++ blocker.py
@@ -5,4 +5,13 @@ root.geometry('500x300')
 root.resizable(0,0)
 root.title("Website Blocker")
 
+Label(root, text='WEBSITE BLOCKER', font='arial 20 bold').pack()
+Label(root, text='Subhayu', font='arial 20 bold').pack(side=BOTTOM)
+
+host_path = 'C:\Windows\System32\drivers\etc\hosts'
+ip_address = '127.0.0.1'
+Label(root, text='Enter Website :', font='arial 13 bold').place(x=5, y=60)
+Websites = Text(root, font='arial 10', height='2', width='40', wrap=WORD, padx=5, pady=5)
+Websites.place(x=140, y=60)
+
 root.mainloop()
