d2ccf1628782a9fafb7a111b4aa4ac90e55620fa
subhayuroy <subhayu.roy2001@gmail.com>
2020-10-18 12:10:41 +0530

    Creating Display window

---
+++ blocker.py
@@ -0,0 +1,8 @@
+from tkinter import *
+
+root = Tk()
+root.geometry('500x300')
+root.resizable(0,0)
+root.title("Website Blocker")
+
+root.mainloop()
---
+++ request.py
@@ -0,0 +1,14 @@
+import requests
+from io import BytesIO
+from PIL import Image
+
+r = requests.get("https://wallpapercave.com/wp/BcXToWf.jpg")
+print("Status code:", r.status_code)
+image = Image.open(BytesIO(r.content))
+print(image.size, image.format, image.mode)
+path = "./image." + image.format
+
+try:
+    image.save(path, image.format)
+except IOError:
+    print("Cannot save image.")
